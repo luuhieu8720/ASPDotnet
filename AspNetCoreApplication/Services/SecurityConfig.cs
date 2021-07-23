4cffe7892f589831d15de78d02282499b67f7836
luuhieu8720 <lethiluuhieu@gmail.com>
2021-07-23 09:09:51 +0700

    Update permission

--- AspNetCoreApplication/Services/SecurityConfig.cs
+++ AspNetCoreApplication/Services/SecurityConfig.cs
@@ -17,6 +17,8 @@ namespace AspNetCoreApplication.Services
             options.RequireHttpsMetadata = false;
             options.TokenValidationParameters = new TokenValidationParameters
             {
+                RequireExpirationTime = true,
+                RequireSignedTokens = true,
                 ValidateIssuerSigningKey = true,
                 IssuerSigningKey = new SymmetricSecurityKey(key),
                 ValidateIssuer = false,
