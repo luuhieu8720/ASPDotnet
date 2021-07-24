282cac191aa3a52b0b2a1c6fc435b0dbf1a8af18
luuhieu8720 <lethiluuhieu@gmail.com>
2021-07-24 16:07:58 +0700

    refactoring

--- AspNetCoreApplication/Models/BaseModel.cs
+++ AspNetCoreApplication/Models/BaseModel.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace AspNetCoreApplication.Repositories
+namespace AspNetCoreApplication.Models
 {
     public class BaseModel
     {
