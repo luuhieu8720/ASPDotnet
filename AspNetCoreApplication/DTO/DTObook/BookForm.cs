6ae59fd945ce404eae136b849ce8476207b864ad
luuhieu8720 <lethiluuhieu@gmail.com>
2021-08-03 12:03:58 +0700

    Add creator for book

--- AspNetCoreApplication/DTO/DTObook/BookForm.cs
+++ AspNetCoreApplication/DTO/DTObook/BookForm.cs
@@ -26,6 +26,10 @@ namespace AspNetCoreApplication.DTO.DTOBook
 
         public Author Author { get; set; }
 
+        public int UserId { get; set; }
+
+        public User User { get; set; }
+
         public string Cover { get; set; }
 
         public List<BookCategory> Categories { get; set; }
