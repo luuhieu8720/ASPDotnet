8f3e78173dcc306ee0e3f783cf8603f579865aff
Vo Quang Hoa <hvquang@openwt.com>
2021-08-11 23:12:57 +0700

    Update deploy file

--- AspNetCoreApplication/deploy_heroku.bat
+++ AspNetCoreApplication/deploy_heroku.bat
@@ -4,13 +4,13 @@ echo BUILD.........................
 docker build -t registry.heroku.com/hbookstore/web .
 
 echo PUSH..........................
-call heroku container:push web
+call heroku container:push web -a hbookstore
 
 echo RELEASE.......................
-call heroku container:release web
+call heroku container:release web -a hbookstore
 
 
 echo OPEN..........................
-call heroku open
+call heroku open -a hbookstore
 
 echo DONE..........................
